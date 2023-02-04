69627de512248b743d7ca731b00bfa9bb3dd4b31
Brianna Zamora <brianna.zamora@rewardstyle.com>
2023-02-04 11:32:46 -0800

    Add code

--- Sources/KeyboardHeightObserver/KeyboardHeightObserver.swift
+++ Sources/KeyboardHeightObserver/KeyboardHeightObserver.swift
@@ -1,6 +1,45 @@
-public struct KeyboardHeightObserver {
-    public private(set) var text = "Hello, World!"
+//
+//  KeyboardHeightObserver.swift
+//  Chat
+//
+//  Created by Bri on 1/15/22.
+//
 
-    public init() {
+import Combine
+import SwiftUI
+
+#if os(iOS)
+public class KeyboardHeightObserver: ObservableObject {
+
+    @Published public var height: CGFloat = 0
+
+    fileprivate var keyboardHeightPublisher: AnyPublisher<CGFloat, Never> {
+        Publishers.Merge(
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillShowNotification)
+                .compactMap { $0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue }
+                .map { $0.cgRectValue.height },
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillHideNotification)
+                .map { _ in CGFloat(0) }
+        )
+        .eraseToAnyPublisher()
+    }
+
+    fileprivate var heightSink: AnyCancellable?
+
+    public func setSink() {
+        heightSink = keyboardHeightPublisher.sink{ newHeight in
+            Task {
+                await self.set(newHeight: newHeight)
+            }
+        }
+    }
+
+    @MainActor fileprivate func set(newHeight: CGFloat) {
+        withAnimation(.spring()) {
+            self.height = height
+        }
     }
 }
+#endif
--- Tests/KeyboardHeightObserverTests/KeyboardHeightObserverTests.swift
+++ Tests/KeyboardHeightObserverTests/KeyboardHeightObserverTests.swift
@@ -2,10 +2,7 @@ import XCTest
 @testable import KeyboardHeightObserver
 
 final class KeyboardHeightObserverTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(KeyboardHeightObserver().text, "Hello, World!")
+    func testInit() throws {
+        XCTAssertEqual(KeyboardHeightObserver().height, 0)
     }
 }
